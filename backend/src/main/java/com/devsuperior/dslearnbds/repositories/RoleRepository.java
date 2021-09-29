686c24932ee7d383a0786a99be2081d57eec8aa0
rqguzman <rafael.guzman@outlook.com>
2021-09-29 12:54:43 -0300

    :sparkles: Feat(Data Access layer): Add all repositories

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/repositories/RoleRepository.java
@@ -0,0 +1,10 @@
+package com.devsuperior.dslearnbds.repositories;
+
+import com.devsuperior.dslearnbds.entities.Role;
+import com.devsuperior.dslearnbds.entities.pk.EnrollmentPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, EnrollmentPK> {
+}
